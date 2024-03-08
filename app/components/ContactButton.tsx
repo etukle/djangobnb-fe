a9b30587447befe5e89c9911f164368a10d9363a
Evren Tukle <tukleevren@gmail.com>
2024-03-08 11:24:06 +0000

    Implements Ladnlord Details Page.

---
+++ app/components/ContactButton.tsx
@@ -0,0 +1,9 @@
+const ContactButton = () => {
+  return (
+    <div className="mt-6 py-4 px-6 cursor-pointer bg-airbnb hover:bg-airbnb-dark text-white rounded-xl transition">
+      Contact Button
+    </div>
+  );
+};
+
+export default ContactButton;
