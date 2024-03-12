d96dac43a88a0e1f4e5c87efaaf72491eeb91213
Evren Tukle <tukleevren@gmail.com>
2024-03-12 14:23:24 +0000

    Implements user menu popup.

--- app/components/navbar/UserNav.tsx
+++ app/components/navbar/UserNav.tsx
@@ -1,7 +1,15 @@
+'use client';
+
+import { useState } from 'react';
+
+import MenuLink from './MenuLink';
+
 const UserNav = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
     <div className="p-2 relative inline-block border rounded-full">
-      <button className="flex items-center">
+      <button className="flex items-center" onClick={() => setIsOpen(!isOpen)}>
         <svg
           fill="none"
           viewBox="0 0 24 24"
@@ -30,6 +38,19 @@ const UserNav = () => {
           />
         </svg>
       </button>
+
+      {isOpen && (
+        <div className="w-[220px] absolute top-[60px] right-0 bg-white border rounded-xl shadow-md flex-col cursor-pointer">
+          <MenuLink
+            label="Log in"
+            onClick={() => console.log('Menu Button Clicked')}
+          />
+          <MenuLink
+            label="Sign up"
+            onClick={() => console.log('Menu Button Clicked')}
+          />
+        </div>
+      )}
     </div>
   );
 };
